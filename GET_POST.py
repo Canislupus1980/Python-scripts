875259c772220520e0b65b39b715a497f500adc1
Siarhei Buhtiyarov <86954730+Canislupus1980@users.noreply.github.com>
2022-09-29 17:00:23 +0300

    Create GET_POST.py

---
+++ GET_POST.py
@@ -0,0 +1,41 @@
+# GET-request
+
+# from urllib import request
+
+# myUrl = "sms.becloud.by"
+
+# answer = request.urlopen(myUrl)
+# mytext1 = answer.realines()
+# mytext2 = answer.read()
+
+# print(answer)
+# print(mytext2)
+
+# for line in mytext1:
+#     print(line)
+
+# POST-request
+
+from urllib import request, parse
+import sys
+
+myUrl = "https://www.google.com/search?"
+value = {'q': 'becloud'}
+
+myheader = {}
+myheader['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+
+
+try:
+    mydata = parse.urlencode(value)
+    print(mydata)
+    myUrl = myUrl + mydata
+    req = request.Request(myUrl, headers=myheader)
+    otvet = request.urlopen(req)
+    answer = request.urlopen(req)
+    answer = answer.readlines()
+    for line in answer:
+        print(line)
+except Exception:        
+        print("Error occuried during web request!!")
+        print(sys.exc_info()[1])
